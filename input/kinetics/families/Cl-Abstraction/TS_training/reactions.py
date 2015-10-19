1e1c7bf5b0e8f666c18041fa5ed210a89396c89a
Pierre Bhoorasingh <bhoorasingh.p@husky.neu.edu>
2015-10-19 18:15:23 -0400

    Add TS_training reactions and groups for Cl-Abstraction

---
+++ input/kinetics/families/Cl-Abstraction/TS_training/reactions.py
@@ -0,0 +1,96 @@
+entry(
+    index = 1,
+    label = "C2H5 + ClH <=> C2ClH5 + H",
+    degeneracy = 1,
+    distances = DistanceData(
+        distances = {'d12': 1.649, 'd13': 3.598, 'd23': 1.949},
+        method = 'm062x/mg3s',
+    ),
+    rank = 3,
+    shortDesc = u"""Manual M06-2X/MG3S calculation.""",
+)
+
+entry(
+    index = 2,
+    label = "Cl + ClH <=> Cl2 + H",
+    degeneracy = 1,
+    distances = DistanceData(
+        distances = {'d12': 2.067, 'd13': 4.096, 'd23': 2.030},
+        method = 'm062x/mg3s',
+    ),
+    rank = 3,
+    shortDesc = u"""Manual M06-2X/MG3S calculation.""",
+)
+ 
+entry(
+    index = 3,
+    label = "Cl3Si + ClH <=> Cl4Si + H",
+    degeneracy = 1,
+    distances = DistanceData(
+        distances = {'d12': 1.536, 'd13': 3.803, 'd23': 2.267},
+        method = 'm062x/mg3s',
+    ),
+    rank = 3,
+    shortDesc = u"""Manual M06-2X/MG3S calculation.""",
+)
+
+entry(
+    index = 4,
+    label = "C2H3 + ClH <=> C2ClH3 + H",
+    degeneracy = 1,
+    distances = DistanceData(
+        distances = {'d12': 1.561, 'd13': 3.486, 'd23': 1.926},
+        method = 'm062x/mg3s',
+    ),
+    rank = 3,
+    shortDesc = u"""Manual M06-2X/MG3S calculation.""",
+)
+
+entry(
+    index = 5,
+    label = "C2ClH5 + H <=> C2H5 + ClH",
+    degeneracy = 1,
+    distances = DistanceData(
+        distances = {'d12': 1.949, 'd13': 3.598, 'd23': 1.649},
+        method = 'm062x/mg3s',
+    ),
+    rank = 3,
+    shortDesc = u"""Reverse reaction for reaction index 1.""",
+)
+
+entry(
+    index = 6,
+    label = "Cl2 + H <=> Cl + ClH",
+    degeneracy = 1,
+    distances = DistanceData(
+        distances = {'d12': 2.030, 'd13': 4.096, 'd23': 2.067},
+        method = 'm062x/mg3s',
+    ),
+    rank = 3,
+    shortDesc = u"""Reverse reaction for reaction index 2.""",
+)
+ 
+entry(
+    index = 7,
+    label = "Cl4Si + H <=> Cl3Si + ClH",
+    degeneracy = 1,
+    distances = DistanceData(
+        distances = {'d12': 2.267, 'd13': 3.803, 'd23': 1.536},
+        method = 'm062x/mg3s',
+    ),
+    rank = 3,
+    shortDesc = u"""Reverse reaction for reaction index 3.""",
+)
+
+entry(
+    index = 8,
+    label = "C2ClH3 + H <=> C2H3 + ClH",
+    degeneracy = 1,
+    distances = DistanceData(
+        distances = {'d12': 1.926, 'd13': 3.486, 'd23': 1.561},
+        method = 'm062x/mg3s',
+    ),
+    rank = 3,
+    shortDesc = u"""Reverse reaction for reaction index 4.""",
+)
+
